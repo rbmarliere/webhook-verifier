09825e8b77245adca47841f6a4fcf330834f26aa
Ricardo B. Marliere <ricardo@marliere.net>
2023-11-16 18:38:46 -0300

    feat: add custom_logger wrapper and -verbose flag

--- server.go
+++ server.go
@@ -13,47 +13,60 @@ import (
 	"flag"
 )
 
+type custom_logger struct {
+	verbose bool
+	*log.Logger
+}
+
+func (l *custom_logger) Println(v ...interface{}) {
+	if l.verbose {
+		l.Logger.Println(v...)
+	}
+}
+
+var logger = &custom_logger{false, log.New(os.Stdout, "", 0)}
+
 func parseHeaders(w http.ResponseWriter, r *http.Request) {
 	secret := r.Header.Get("Secret")
 	if secret == "" {
-		log.Println("Secret not found")
+		logger.Println("Secret not found")
 		return
 	}
-	log.Println("Found Secret: " + secret)
+	logger.Println("Found Secret: " + secret)
 
 	project_root := r.Header.Get("Project-Root")
 	if project_root == "" {
-		log.Println("Project-Root not found")
+		logger.Println("Project-Root not found")
 		return
 	}
 
-	log.Println("Found Project-Root: " + project_root)
+	logger.Println("Found Project-Root: " + project_root)
 	_, error := os.Stat(project_root)
 	if error != nil {
-		log.Println("Project-Root path does not exist")
+		logger.Println("Project-Root path does not exist")
 		return
 	}
 
 	expected_signature := r.Header.Get("X-Hub-Signature-256")
 	if expected_signature == "" {
-		log.Println("X-Hub-Signature-256 not found")
+		logger.Println("X-Hub-Signature-256 not found")
 		return
 	}
-	log.Println("Found X-Hub-Signature-256: " + expected_signature)
+	logger.Println("Found X-Hub-Signature-256: " + expected_signature)
 
 	payload, error := io.ReadAll(r.Body)
 	if payload == nil || error != nil {
-		log.Println("Payload not found")
+		logger.Println("Payload not found")
 		return
 	}
 
 	digest := hmac.New(sha256.New, []byte(secret))
 	digest.Write(payload)
 	signature := fmt.Sprintf("sha256=%x", digest.Sum(nil))
-	log.Println("Signature: " + expected_signature)
+	logger.Println("Signature: " + expected_signature)
 
 	if signature != expected_signature {
-		log.Println("Signatures do not match")
+		logger.Println("Signatures do not match")
 		return
 	}
 
@@ -63,14 +76,17 @@ func parseHeaders(w http.ResponseWriter, r *http.Request) {
 	cmd.Stderr = &out
 	error = cmd.Run()
 	if error != nil {
-		log.Println(error)
+		logger.Println(error)
 	}
-	log.Println(out.String())
+	logger.Println(out.String())
 }
 
 func main() {
 	port := flag.String("port", "32777", "port to listen on")
+	verbose := flag.Bool("verbose", false, "verbose logging")
 	flag.Parse()
+
+	logger.verbose = *verbose
 
 	http.HandleFunc("/", parseHeaders)
 	http.ListenAndServe(":" + *port, nil)
