aec06bf7329d13c0807714fb5712c0433176085a
Ricardo B. Marliere <ricardo@marliere.net>
2023-11-16 16:44:10 -0300

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rbmarliere/webhook-verifier
+
+go 1.21.4
---
+++ server.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+	"os/exec"
+	"crypto/hmac"
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+func parseHeaders(w http.ResponseWriter, r *http.Request) {
+	secret := r.Header.Get("Secret")
+	if secret == "" {
+		log.Fatal("Secret not found")
+	}
+
+	project_root := r.Header.Get("Project-Root")
+	if project_root == "" {
+		log.Fatal("Project-Root not found")
+	}
+
+	signature := r.Header.Get("X-Hub-Signature-256")
+	if signature == "" {
+		log.Fatal("X-Hub-Signature-256 not found")
+	}
+	fmt.Println(signature)
+
+	payload, error := io.ReadAll(r.Body)
+	if error != nil {
+		return
+	}
+
+	digest := hmac.New(sha256.New, []byte(secret))
+	digest.Write(payload)
+	expected := fmt.Sprintf("sha256=%x", digest.Sum(nil))
+	fmt.Println(expected)
+	if expected != signature {
+		log.Fatal("Signatures do not match")
+	}
+
+	cmd := exec.Command("git", "-C", project_root, "pull")
+	var out strings.Builder
+	cmd.Stdout = &out
+	cmd.Stderr = &out
+	err := cmd.Run()
+	fmt.Println(out.String())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", parseHeaders)
+	http.ListenAndServe(":8080", nil)
+}
